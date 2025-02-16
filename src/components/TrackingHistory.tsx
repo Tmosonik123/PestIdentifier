4aea499af553e6c8d9c346ac6b9eb19b96e9674f
Tmosonik123 <mosoniktitus4@gmail.com>
2025-02-16 22:08:26 +0000

    added tracking history

--- src/components/TrackingHistory.tsx
+++ src/components/TrackingHistory.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
